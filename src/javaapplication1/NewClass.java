162fa88ce8a4a63a11a3afc73ca13f77bbc0e477
crist <crist@Kristys_PC>
2022-04-22 15:15:48 -0600

    El anexo de la clase Alumno

---
+++ src/javaapplication1/NewClass.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author crist
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        /*
+        Caracteres especiales
+        
+        \n = Salto de linea
+        
+        */
+        System.out.println("Hola Mundo!" );
+        System.out.println("- Valor Numerico: "+(10+12.34)+"$ \nEstado: "+true+" - Habilitado");
+        System.out.print("Datos de alumnos\nCuenta: 20911036\nNombre Alumno: Erick Amaya\nCarrera: Ingenieria Sistemas\n");        
+        
+    }
+    // println sirve para hacer espacio
+}
+
+   
